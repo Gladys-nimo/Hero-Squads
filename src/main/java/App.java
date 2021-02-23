6b6552a7ecbf5000aaefc520cd3964cbaeed8703
Gladys-nimo <wkanyora10@gmail.com>
2021-02-23 14:51:37 -0800

    added a route to squad-form in my app.java file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,5 +31,18 @@ public class App {
         return new ModelAndView(model, "hero.hbs");
     }, new HandlebarsTemplateEngine());
 
+        get("/new/:id",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            int idOfHero = Integer.parseInt(req.params(":id"));
+            Hero foundHero = Hero.findById(idOfHero);
+            model.put("hero",foundHero);
+            return new ModelAndView(model, "more.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad-form",(request, restore) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
